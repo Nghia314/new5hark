9e3037ffd0c079e97980f73a11ff5f1d65d26d59
sharkbyte <sgquin@gmail.com>
2022-06-03 17:13:01 -0700

    get all activities route and controller serve up JSON

--- server/server.js
+++ server/server.js
@@ -12,6 +12,6 @@ app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`🚀   Houston We Have A Sever 🚀`);
+    console.log(`🚀  Houston We Have A Sever 🚀`);
   });
 });
