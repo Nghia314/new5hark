33f762b06d4a457d7c5cc40f89cd21c0aee41f80
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-04 12:38:59 -0700

    Added second example data

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -4,8 +4,7 @@ import Chart from './Chart';
 function Container() {
 
   return (
-    <>
-    <div className="container mx-auto">
+    <div>
       <div className="card max-w-3xl p-3">
         <div className="card-body border-solid border-2 border-sky-800 bg-slate-800 border-solid rounded">
           <h2 className="text-center text-3xl font-bold">Monthly Chart Summary</h2>
@@ -13,7 +12,6 @@ function Container() {
         </div>
       </div>
     </div>
-    </>
   );
 }
 
