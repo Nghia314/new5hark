3e33302d16fcc3ed7a0781c9b5011b909bbb7612
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-04 19:34:52 -0700

    Added homepage content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Pages
 import Home from "./pages/Home";
@@ -7,24 +6,22 @@ import Login from "./pages/Login";
 import YourDay from "./pages/YourDay";
 import Dashboard from "./pages/Dashboard";
 // Components
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
-        <Route path='/YourDay' element={
-          <YourDay/>
-          }/>
+        <Route path='/YourDay' element={<YourDay/>}/>
         <Route path='/Dashboard' element={<Dashboard/>}/>
         <Route path='*' element={
-          <h1 className="text-center text-3xl font-bold">Wrong Route!</h1>
+          <h1 className="text-center text-3xl font-bold">404 Wrong Route!</h1>
         }/>
       </Routes>
     </BrowserRouter>
-
   );
 }
 
