6b182e012e50811cc951f8eeb8f997a5953a1aa2
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-05 13:47:47 -0700

    Added initial layout

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,12 +3,35 @@ import React from "react";
 function Dashboard() {
   return (
     <>
-    <div className="grid row-2 m-3 gap-3">
-      <div className="card col bg-neutral p-3">
-        <h1>Test</h1>
+    <div className="grid md:grid-rows-2-md md:grid-flow-col m-3 gap-3 text-center">
+      <div className="card md:col-span-8 bg-neutral w-full p-3 text-center">
+        <h2 className="card-title">Build your Day</h2>
+        <div className="grid md:grid-cols-2 gap-3">
+          <div>
+            <h1 className="mb-3">Sid's Activities</h1>
+            <div className="grid md:grid-cols-2 gap-3">
+              <button className="btn bg-base-100 w-full truncate">Walk</button>
+              <button className="btn bg-base-100 w-full truncate">Walk</button>
+              <button className="btn bg-base-100 w-full truncate">Walk</button>
+            </div>
+          </div>
+          <div >
+
+          </div>
+        </div>
       </div>
-      <div className="card col bg-neutral p-3">
-        <h1>Test</h1>
+      <div className="card md:col-span-4 bg-neutral w-full p-3">
+        <h2 className="card-title">My Day</h2>
+        <div className="grid md:grid-cols-2 gap-3 my-3">
+          <button className="btn btn-secondary w-full truncate">Walk</button>
+          <button className="btn btn-secondary w-full truncate">Walk</button>
+          <button className="btn btn-secondary w-full truncate">Walk</button>
+          <button className="btn btn-secondary w-full truncate">Walk</button>
+          <button className="btn btn-secondary w-full truncate">Walk</button>
+          <button className="btn btn-secondary w-full truncate">Walk</button>
+          <button className="btn btn-secondary w-full truncate">Walk</button>
+        </div>
+        <button className="btn btn-primary">Start my Day!</button>
       </div>
     </div>
     </>
