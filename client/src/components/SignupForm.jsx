48a2ef8acab609c2986e88142632561907beae42
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-05 10:51:31 -0700

    Added stylings to signup form

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -6,15 +6,17 @@ function SignupForm() {
 
   return (
     <>
-      <div className="block p-6 rounded-lg text-light shadow-lg bg-neutral max-w-md">
+      <div className="block p-6 rounded-lg text-light shadow-lg bg-neutral w-full max-w-md">
+        <h2 className="text-center text-3xl font-bold mb-6">Sign up!</h2>
         <form>
           <div className="form-group mb-6">
             <input
               type="text"
-              className="form-control block w-full px-3 py-1.5 m-0
-                text-base font-normal text-light
-                bg-base-100 bg-clip-padding border-gray-300 rounded transition ease-in-out
-                focus:bg-base-100 focus:outline-primary focus:outline-none"
+              className="input input-bordered w-full focus:input-primary"
+              // className="form-control block w-full px-3 py-1.5 m-0
+              //   text-base font-normal text-light
+              //   bg-base-100 bg-clip-padding border-gray-300 rounded transition ease-in-out
+              //   focus:bg-base-100 focus:outline-primary focus:outline-none"
               name="name"
               aria-describedby="emailHelp123"
               placeholder="Name"
@@ -24,10 +26,11 @@ function SignupForm() {
           <div className="form-group mb-6">
             <input
               type="email"
-              className="form-control block w-full px-3 py-1.5 m-0
-                text-base font-normal text-light
-                bg-base-100 bg-clip-padding border-gray-300 rounded transition ease-in-out
-                focus:bg-base-100 focus:outline-primary focus:outline-none"
+              className="input input-bordered w-full focus:input-primary"
+              // className="form-control block w-full px-3 py-1.5 m-0
+              //   text-base font-normal text-light
+              //   bg-base-100 bg-clip-padding border-gray-300 rounded transition ease-in-out
+              //   focus:bg-base-100 focus:outline-primary focus:outline-none"
               id="exampleInput125"
               placeholder="Email"
             />
@@ -35,17 +38,21 @@ function SignupForm() {
           <div className="form-group mb-6">
             <input
               type="password"
-              className="form-control block w-full px-3 py-1.5 m-0
-                text-base font-normal text-light
-                bg-base-100 bg-clip-padding border-gray-300 rounded transition ease-in-out
-                focus:bg-base-100 focus:outline-primary focus:outline-none"
+              className="input input-bordered w-full focus:input-primary"
+              // className="form-control block w-full px-3 py-1.5 m-0
+              //   text-base font-normal text-light
+              //   bg-base-100 bg-clip-padding border-gray-300 rounded transition ease-in-out
+              //   focus:bg-base-100 focus:outline-primary focus:outline-none"
               id="exampleInput126"
               placeholder="Password"
             />
           </div>
           <button
             type="submit"
-            className=" w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            className="btn btn-primary w-full"
+            // className="w-full px-6 py-2.5 bg-primary text-white font-medium text-xs leading-tight uppercase rounded shadow-md
+            // hover:bg-blue-700 hover:shadow-lg
+            // focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
             Sign up
           </button>
