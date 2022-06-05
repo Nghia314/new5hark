f9a375ab1a0526d4bed92dffd658eda800ce2b49
Nghia Le <davidle314@hotmail.com>
2022-06-05 11:14:50 -0700

    finish coding for login form

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -32,8 +32,11 @@ async login({ body }, res) {
   const user = await User.create(body);
 
   if (!user) {
-    
+    return res.status(400).json({ message: "Something is wrong! "});
+
   }
-}
+  const token = signToken(user);
+  res.json({ token, user });
+},
 
 };
