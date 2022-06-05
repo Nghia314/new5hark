0ba0d5f697c443a91fb526e1218e7b4d513b01a4
sharkbyte <sgquin@gmail.com>
2022-06-04 19:09:51 -0700

    login form template added

---
+++ client/src/pages/ComponentTester.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Loginform from "../components/Loginform";
+
+function ComponentTester() {
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold">Test Components Here!</h1>
+      <Loginform />
+    </>
+  );
+}
+
+export default ComponentTester;
