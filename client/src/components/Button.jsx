c035be5f6d3eb3c92e8ccc6f8de3386ce55c46d5
sharkbyte <sgquin@gmail.com>
2022-06-06 16:54:18 -0700

    form adds new activities

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-function Button({ onClick, color, activity }) {
+function Button({ key, onClick, color, activity }) {
   return (
     // <div className="tooltip" datatip={activity.description}>
-    <button onClick={onClick} className={`btn btn-${color} w-full truncate`}>
-      {activity}
-    </button>
+    <>
+      <button
+        key={key}
+        onClick={onClick}
+        className={`btn btn-${color} w-full truncate`}
+      >
+        {activity}
+      </button>
+    </>
     // </div>
   );
 }
--- client/src/components/CreateActivityForm.jsx
+++ client/src/components/CreateActivityForm.jsx
@@ -1,27 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Security from "../utils/security";
+import { createActivity } from "../utils/API";
 
 function CreateActivityForm() {
+  const [formState, setForm] = useState({});
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const token = Security.loggedIn() ? Security.getToken() : null;
+    if (!token) {
+      return false;
+    }
+    try {
+      const res = await createActivity(formState, token);
+      if (!res.ok) {
+        alert("something went wrong while creating activity");
+      }
+      const newAct = await res.json();
+      alert("New Activity Created");
+    } catch (err) {
+      alert("Something went wrong with whole function");
+    }
+  };
+
+  const handleFormChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setForm({ ...formState, [name]: value });
+  };
+
   return (
     <div>
       <h1 className="mb-3">Create Activity</h1>
       {/* <Form / handleNewActivityForm></Form> */}
-      <form>
+      <form onChange={handleFormChange} onSubmit={handleFormSubmit}>
         <div className="input-group">
           <input
             className="input input-primary input-bordered w-full"
             type="text"
             placeholder="Activity name"
+            name="name"
           />
-          <button type="submit" className="btn btn-primary font-bold">
-            +
-          </button>
         </div>
 
         <input
           className="input input-primary input-bordered w-full"
           type="text"
           placeholder="Description"
+          name="description"
         />
+        <button type="submit" className="btn btn-primary font-bold">
+          Add Activity
+        </button>
       </form>
     </div>
   );
