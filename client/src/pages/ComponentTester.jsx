78385622252eed7f4cfd748205ffc263c5887fff
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-06 14:47:52 -0700

    Button component takes in props

--- client/src/pages/ComponentTester.jsx
+++ client/src/pages/ComponentTester.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import Loginform from "../components/Loginform";
 import SignupForm from "../components/SignupForm";
+import Button from "../components/Button";
 
 function ComponentTester() {
+  function itWorks() {
+    console.log("it works");
+  }
+
+  const obj = {
+    _id: 12341234,
+    name: "walk",
+    description: "i like to walk at night"
+  }
+
   return (
     <>
       <h1 className="text-center text-3xl font-bold">Test Components Here!</h1>
-      <Loginform />
-      <SignupForm />
+      {/* <Loginform />
+      <SignupForm /> */}
+      <Button 
+      activity={obj.name}
+      onClick={itWorks}
+      color={"primary"}
+      />
     </>
   );
 }
