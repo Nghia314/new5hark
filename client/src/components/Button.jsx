d56eb69959089930e908a6357d39d994ddc9496a
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-07 13:41:20 -0700

    Added line-wrap in buttons

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function Button({ onClick, color, activity }) {
   return (
     // <div className="tooltip" datatip={activity.description}>
-    <button onClick={onClick} className={`btn btn-${color} w-full truncate`}>
-      {activity}
+    <button onClick={onClick} className={`btn btn-${color} w-full`}>
+      <p className="line-clamp-2">
+        {activity}
+      </p>
     </button>
     // </div>
   );
