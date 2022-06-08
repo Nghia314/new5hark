96e0a856c8add8642a86345b74395f3543c6b9fb
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-08 16:40:46 -0700

    Added YourDayActivities component

--- client/src/pages/YourDay.jsx
+++ client/src/pages/YourDay.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Chart from "../components/Chart"
 import Navbar from "../components/Navbar";
 import { useLocation } from "react-router-dom";
+import YourDayActivities from "../components/YourDayActivities";
 
 function YourDay() {
   console.log("This is what you're sending over");
@@ -16,11 +17,10 @@ function YourDay() {
         <div className="card bg-neutral text-center">
           <h2 className="card-header text-neutral p-3 bg-primary">Today</h2>
           <div className="p-3 grid-flow-col">
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 my-3">
-              {/* <div className="input-group w-full">
-                <button className="btn btn-primary btn-sm w-full text-xl">Walk</button>
-                <input className="checkbox checkbox-primary checkbox-lg" type="checkbox"/>
-              </div> */}
+            <YourDayActivities
+              // Send in data here
+            />
+            {/* <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 my-3">
               <div className="btn btn-outline btn-primary w-full text-xl truncate">Walk</div>
               <div className="btn btn-outline btn-primary w-full text-xl truncate">Walk</div>
               <div className="btn btn-outline btn-primary w-full text-xl truncate">Walk</div>
@@ -35,7 +35,7 @@ function YourDay() {
               <div className="btn btn-outline btn-primary w-full text-xl truncate">Walk</div>
               <div className="btn btn-outline btn-primary w-full text-xl truncate">Walk</div>
               <div className="btn btn-outline btn-primary w-full text-xl truncate">Walk</div>
-            </div>
+            </div> */}
             <div className="p-3">
               <h3 className="text-4xl">You've done</h3>
               <h1 className="text-6xl text-primary">3</h1>
