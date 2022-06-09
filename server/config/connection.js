59730dc80beaed3bdb4d4e0a821f88d541042eb9
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-09 10:42:00 -0700

    Added mongoDB URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,11 @@
-const mongoose = require("mongoose");
+const { connect, connection } = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/new5hark", {
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/new5hark';
+
+connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-module.exports = mongoose.connection;
+module.exports = connection;
