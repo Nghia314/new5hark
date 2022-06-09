290d5aec976ea2aca397fa167cf331e31d960324
sharkbyte <sgquin@gmail.com>
2022-06-09 14:40:14 -0700

    Navigate to dashboard on login

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const db = require("./config/connection");
 
 const routes = require("./routes");
@@ -10,10 +10,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
 app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`🚀  Houston We Have A Sever 🚀 on port ${PORT}`);
+    console.log(`🚀  Houston We Have A Sever on port ${PORT} 🚀) `);
   });
 });
