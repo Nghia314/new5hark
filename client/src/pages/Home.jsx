51bfaf721dc636a290261fe9f9f67ea96def7b2f
Nghia Le <davidle314@hotmail.com>
2022-06-08 18:28:24 -0700

    do the back ground for Login form

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import Loginform from "../components/Loginform";
 import styled, { keyframes } from "styled-components";
 import { LoadingButton } from "../components/AllSvgs";
+import  Shark  from "../Assets/Shark.png"
 // style for main container
 const Maincontainer = styled.div`
   background: ${(props) => props.theme.body};
-  background-image: url(https://api.lorem.space/image?w=1300%&h=1000);
+  background-image: ${(Shark)}
   width: 100vw;
   height: 100vh;
   overflow: hidden;
@@ -25,6 +26,21 @@ const Container = styled.div`
 padding: 2rem
 justify-content: center
 `;
+const float = styled.div`
+0% { transform: translateY(-10px) }
+50% { transform: translateY(15px) translateX(15px) }
+100% { transform: translateY(-10px) }`
+
+const Sharklogo= styled.div`
+position: absolute;
+top: 10%;
+right: 5%;
+width: 20vw;
+animation: ${float} 4s ease infinite;
+img{
+  width: 100%;
+  height: auto;
+}`
 
 const rotate = keyframes`
 from {
