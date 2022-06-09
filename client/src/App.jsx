16386bf843be4e1c03fa03c1cc8a644999d2f43d
sharkbyte <sgquin@gmail.com>
2022-06-08 19:11:49 -0700

    logging in and logging out work, home conditionally links to dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,6 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Signup" element={<SignUp />} />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Navbar() {
+import Security from "../utils/security";
 
-  return (
-<div className="navbar bg-neutral">
-  <div className="flex-1">
-    <Link className="btn btn-ghost normal-case text-xl" to="/">Home</Link>
-    <Link className="btn btn-ghost normal-case text-xl" to="/YourDay">Your Day</Link>
-    <Link className="btn btn-ghost normal-case text-xl" to="/Dashboard">Dashboard</Link>
-    <Link className="btn btn-ghost normal-case text-xl" to="/login">Login</Link>
-  </div>
-  <div className="flex-none">
-    <button className="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-    </button>
-  </div>
-</div>
-  );
+function Navbar() {
+  if (Security.loggedIn) {
+    return (
+      <div className="navbar bg-neutral">
+        <div className="flex-1">
+          <Link className="btn btn-ghost normal-case text-xl" to="/">
+            Home
+          </Link>
+          {Security.loggedIn() ? (
+            <>
+              <Link
+                className="btn btn-ghost normal-case text-xl"
+                to="/Dashboard"
+              >
+                Dashboard
+              </Link>
+              <Link
+                className="btn btn-ghost normal-case text-xl"
+                to="/"
+                onClick={Security.logout}
+              >
+                Logout
+              </Link>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
